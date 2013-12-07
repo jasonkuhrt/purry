4abcd9e61ba1ef4cf66b4de44ccb483d358e7af6
Jason Kuhrt <jasonkuhrt@me.com>
2013-12-07 02:27:18 -0500

    Add benchmark results and tweeked bench runner

--- bench/index.js
+++ bench/index.js
@@ -1,20 +1,19 @@
 var Benchmark = require('benchmark');
 var curry = require('lodash').curry;
 var purry = require('../');
-
-
-
-var suite = new Benchmark.Suite;
-
 var echo_arguments = function(a, b, c, d, e, f, g){
   return [a, b, c, d, e, f, g];
 };
-
 var echo_arguments_curried = curry(echo_arguments);
 var echo_arguments_purried = purry(echo_arguments);
-var maxTime = 0.1;
 
-// add tests
+var maxTime = Number(process.argv[2]) || 10;
+
+
+
+var suite = Benchmark.Suite('All arguments at once');
+var suite_results = [];
+
 suite.add('echo_arguments', function() {
   echo_arguments(1,2,3,4,5,6,7);
 }, {maxTime:maxTime})
@@ -27,12 +26,35 @@ suite.add('echo_arguments', function() {
   echo_arguments_purried(1,2,3,4,5,6,7);
 }, {maxTime:maxTime})
 
-// add listeners
 .on('cycle', function(event) {
-  console.log(String(event.target));
+  suite_results.push(String(event.target));
 })
 .on('complete', function() {
-  console.log('Fastest is ' + this.filter('fastest').pluck('name'));
+  suite_results.forEach(function(x){ console.log(x); });
+  console.log('Fastest is %s\n\n', this.filter('fastest').pluck('name'));
 })
-// run async
+
+.run({ async: true});
+
+
+
+var suite_curries = Benchmark.Suite('Arguments added incrementally');
+var suite_curries_results = [];
+
+suite_curries.add('echo_arguments_curried', function() {
+  echo_arguments_curried(1)(2)(3)(4)(5)(6)(7);
+}, {maxTime:maxTime})
+
+.add('echo_arguments_purried', function() {
+  echo_arguments_purried(1)(2)(3)(4)(5)(6)(7);
+}, {maxTime:maxTime})
+
+.on('cycle', function(event) {
+  suite_curries_results.push(String(event.target));
+})
+.on('complete', function() {
+  suite_curries_results.forEach(function(x){ console.log(x); });
+  console.log('Fastest is %s\n\n', this.filter('fastest').pluck('name'));
+})
+
 .run({ async: true});
