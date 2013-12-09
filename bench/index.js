1de31958e6a391c2ad45d7e195a9ce4019935250
Jason Kuhrt <jasonkuhrt@me.com>
2013-12-09 11:16:52 -0500

    Add partial right and partial left benchmarks

--- bench/index.js
+++ bench/index.js
@@ -1,8 +1,12 @@
 var Benchmark = require('benchmark');
 var curry = require('lodash').curry;
+var partial = require('lodash').partial;
+var partialRight = require('lodash').partialRight;
 var purry = require('../');
-var echo_arguments = function(a, b, c, d, e, f, g){
-  return [a, b, c, d, e, f, g];
+var ___ = purry.___;
+var __ = purry.__;
+var echo_arguments = function(a, b, c, d, e, f){
+  return [a, b, c, d, e, f];
 };
 var echo_arguments_curried = curry(echo_arguments);
 var echo_arguments_purried = purry(echo_arguments);
@@ -15,15 +19,15 @@ var suite = Benchmark.Suite('All arguments at once');
 var suite_results = [];
 
 suite.add('echo_arguments', function() {
-  echo_arguments(1,2,3,4,5,6,7);
+  echo_arguments(1,2,3,4,5,6);
 }, {maxTime:maxTime})
 
 .add('echo_arguments_curried', function() {
-  echo_arguments_curried(1,2,3,4,5,6,7);
+  echo_arguments_curried(1,2,3,4,5,6);
 }, {maxTime:maxTime})
 
 .add('echo_arguments_purried', function() {
-  echo_arguments_purried(1,2,3,4,5,6,7);
+  echo_arguments_purried(1,2,3,4,5,6);
 }, {maxTime:maxTime})
 
 .on('cycle', function(event) {
@@ -42,11 +46,11 @@ var suite_curries = Benchmark.Suite('Arguments added incrementally');
 var suite_curries_results = [];
 
 suite_curries.add('echo_arguments_curried', function() {
-  echo_arguments_curried(1)(2)(3)(4)(5)(6)(7);
+  echo_arguments_curried(1)(2)(3)(4)(5)(6);
 }, {maxTime:maxTime})
 
 .add('echo_arguments_purried', function() {
-  echo_arguments_purried(1)(2)(3)(4)(5)(6)(7);
+  echo_arguments_purried(1)(2)(3)(4)(5)(6);
 }, {maxTime:maxTime})
 
 .on('cycle', function(event) {
@@ -54,6 +58,41 @@ suite_curries.add('echo_arguments_curried', function() {
 })
 .on('complete', function() {
   suite_curries_results.forEach(function(x){ console.log(x); });
+  console.log('Fastest is %s\n\n', this.filter('fastest').pluck('name'));
+})
+
+.run({ async: true});
+
+
+
+var suite_partial = Benchmark.Suite('partial application');
+var suite_partial_results = [];
+
+suite_partial.add('partial(echo_arguments)', function() {
+  partial(partial(partial(partial(partial(partial(echo_arguments, 1), 2), 3), 4), 5), 6)();
+}, {maxTime:maxTime})
+
+.add('partialRight(echo_arguments)', function() {
+  partialRight(partialRight(partialRight(partialRight(partialRight(partialRight(echo_arguments, 6), 5), 4), 3), 2), 1)();
+}, {maxTime:maxTime})
+
+.add('echo_arguments_purried (left)', function() {
+  echo_arguments_purried(1,___)(2,___)(3,___)(4,___)(5,___)(6,___)();
+}, {maxTime:maxTime})
+
+.add('echo_arguments_purried (right)', function() {
+  echo_arguments_purried(___,6)(___,5)(___,4)(___,3)(___,2)(___,1)();
+}, {maxTime:maxTime})
+
+.on('error', function(err){
+  console.error(err)
+})
+
+.on('cycle', function(event) {
+  suite_partial_results.push(String(event.target));
+})
+.on('complete', function() {
+  suite_partial_results.forEach(function(x){ console.log(x); });
   console.log('Fastest is %s\n\n', this.filter('fastest').pluck('name'));
 })
 
