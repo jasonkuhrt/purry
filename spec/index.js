b2cbabd39c930f268679980377eb6b08210acff0
Jason Kuhrt <jasonkuhrt@me.com>
2013-12-10 22:34:32 -0500

    Reorganize spec for greater grainularity and readability

--- spec/index.js
+++ spec/index.js
@@ -6,92 +6,132 @@ purry.installSyntax();
 
 
 
-var test_partialing = function(r, pf){
-  describe('partial applying', function(){
+var test_partialing = function(r, f){
+  describe('partially applying', function(){
 
-    it('may repeatedly shoulder each argument left', function(){
-      r(pf(1,___)(2,___)(3,___)(4,___)(5,___)(6,___)());
+    describe('left', function(){
+      it('delays execution', function(){
+        r(f(1,2,3,4,5,6,___)());
+      });
+
+      it('Each application sends arguments to the left shoulder, in order of application (earlier is farther left)', function(){
+        r(f(1,___)(2,___)(3,___)(4,___)(5,___)(6,___)());
+      });
+
+      it('May argue only some initial parameters', function(){
+        r(f(1,2,3,___)(4,5,6));
+      });
+
+      it('May repeatedly argue some initial parameters', function(){
+        r(f(1,2,3,___)(4,5,___)(6));
+      });
+
+      it('may skip certain initial parameters with holes', function(){
+        r(f(_,2,___)(1,___)(3,4,5,6));
+      });
+
+      it('Available parameters are argued in order during each invocation instance, and reconstructed in sum in order at execution time', function(){
+        // Notice how the second invocation treats the function as if it were 4 params
+        // but the final execution knows how to put 1 at the start and 4,5,6 at the end
+        r(f(_,2,3,___)(1,4,5,6,___)())
+      });
+
     });
 
-    it('may repeatedly shoulder each argument right', function(){
-      r(pf(___,6)(___,5)(___,4)(___,3)(___,2)(___,1)());
+    describe('right', function(){
+      it('delays execution', function(){
+        r(f(___,1,2,3,4,5,6)());
+      });
+
+      it('Each application sends arguments to the right shoulder, in order of application (later is farther right)', function(){
+        r(f(___,6)(___,5)(___,4)(___,3)(___,2)(___,1)());
+      });
+
+      it('May argue only some final parameters', function(){
+        r(f(___,4,5,6)(1,2,3));
+      });
+
+      it('May repeatedly argue some final parameters', function(){
+        r(f(___,4,5,6)(___,2,3)(1));
+      });
+
+      it('may skip certain final parameters with holes', function(){
+        r(f(___,5,_)(___,6)(1,2,3,4));
+      });
+
+      it('Available parameters are argued in order during each invocation instance, and reconstructed in sum in order at execution time', function(){
+        // Notice how the second invocation treats the function as if it were 4 params
+        // but the final execution knows how to put 6 at the end and 1,2,3 at the start
+        r(f(___,4,5,_)(___,1,2,3,6)());
+      });
     });
 
-    it('may shoulder some arguments', function(){
-      r(pf(1,2,3,___)(4,5,6));
-      r(pf(___,4,5,6)(1,2,3));
+    describe('left/right combinations', function(){
+      it('may apply left, then later right', function(){
+        r(f(1,2,3,___)(___,5,6)(4));
+      });
+
+      it('may apply right, then later left', function(){
+        r(f(___,4,5,6)(1,2,___)(3));
+      });
+
+      it('may apply left/right at once', function(){
+        r(f(1,___,6)(2,___,5)(3,___,4)())
+      });
+
+      it('may apply left/right at once with holes', function(){
+        r(f(___,4,5,_)(1,2,3,___,6)())
+        // Similar, latter less verbose:
+        r(f(1,_,3,___,4,_,6)(2,___)(___,5)())
+        r(f(1,_,3,___,4,_,6)(2,___,5)())
+      });
     });
 
-    it('may repeatedly shoulder some arguments', function(){
-      r(pf(1,2,3,___)(4,5,___)(6));
-      r(pf(___,4,5,6)(___,2,3)(1));
+    describe('holes', function(){
+      it('defaults to left shoulder, not strictly checked against param count', function(){
+        // Note: omitting ,___ or ,_,_ after 4
+        r(f(_,2,_,4)(1,3,5,6));
+      });
+
+      it('may skip any parameter', function(){
+        r(f(_,2,3,_,_,_)(_,4,5,6)(1));
+        r(f(_,2,3)(_,4,5)(_,6)(1));
+        r(f(_,2,3)(_,4,5)(1,6));
+      });
+
+      it('Holes count against the param count during invocation thus predictably dropping over-supplied arguments', function(){
+        // 7 is dropped, param count is 4 but 5 args given
+        r(f(_,2,3,_,_,_)(_,4,5,6,7)(1))
+      });
     });
 
-    it('may repeatedly shoulder some arguments in different directions', function(){
-      r(pf(1,2,3,___)(___,5,6)(4));
-      r(pf(___,4,5,6)(1,2,___)(3));
-    });
 
-    it('may delay by partialing holes', function(){
-      r(pf(_)(1,2,3,4,5,6));
-      r(pf(___)(1,2,3,4,5,6));
-    });
+    describe('delaying', function(){
+      it('may delay by partialing holes', function(){
+        r(f(_)(1,2,3,4,5,6));
+        r(f(___)(1,2,3,4,5,6));
+      });
 
-    it('may repeatedly delay', function(){
-      r(pf(_)(_)(_)(_)(_)(1,2,3,4,5,6));
-      r(pf(___)(___)(___)(___)(___)(1,2,3,4,5,6));
-    });
+      it('delays execution repeatedly', function(){
+        r(f(_)(_)(_)(_)(_)(1,2,3,4,5,6));
+        r(f(___)(___)(___)(___)(___)(1,2,3,4,5,6));
+      });
 
-    it('may arbitrarly mix delay and shoulder', function(){
-      r(pf(1,2,___)(_)(3,4,5,6));
-      r(pf(1,2,___)(___)(3,4,5,6));
-    });
+      it('may arbitrarly mix delay and shoulder', function(){
+        r(f(1,2,___)(_)(3,4,5,6));
+        r(f(1,2,___)(___)(3,4,5,6));
+      });
 
-    it('may arbitrarly repeatedly mix delay and shoulder', function(){
-      r(pf(1,2,___)(_)(3,4,___)(_)(5,6,___)());
-      r(pf(1,2,___)(___)(3,4,___)(___)(5,6,___)(___)());
-    });
-
-    it('may shoulder partials', function(){
-      r(pf(_,2,___)(1,___)(3,4,5,6));
-      r(pf(___,5,_)(___,6)(1,2,3,4));
-    });
-
-    it('may partial intermediaries', function(){
-      r(pf(_,2,3,_,_,_)(_,4,5,6)(1))
-      r(pf(_,2,3,___)(_,4,5)(1,6))
-    });
-
-    it('intermediary partial defaults to left shoulder, not strictly checked against param count,', function(){
-      // Note: omitting ,___ or ,_,_ after 4
-      r(pf(_,2,_,4)(1,3,5,6));
-    });
-
-    it('plugs are constants, conflicting indexes from new args are moved opposite the shoulder', function(){
-      // Note: 1,4,... instead of: 1,_,_,4,...
-      r(pf(_,2,3,___)(1,4,5,6,___)())
-      r(pf(___,4,5,_)(___,1,2,3,6)())
-    });
-
-    it('may partial intermediaries, and to either shoulder at once', function(){
-      r(pf(___,4,5,_)(1,2,3,___,6)())
-      r(pf(1,___,6)(2,___,5)(3,___,4)())
-      // Similar, latter less verbose:
-      r(pf(1,_,3,___,4,_,6)(2,___)(___,5)())
-      r(pf(1,_,3,___,4,_,6)(2,___,5)())
+      it('may arbitrarly repeatedly mix delay and shoulder', function(){
+        r(f(1,2,___)(_)(3,4,___)(_)(5,6,___)());
+        r(f(1,2,___)(___)(3,4,___)(___)(5,6,___)(___)());
+      });
     });
 
     it.skip('drops over-partialed arguments right-to-left', function(){
-      r(pf(1,2,3,4,5,6,7,8,9,10,___)())
+      r(f(1,2,3,4,5,6,7,8,9,10,___)())
     });
-
-    it('Holes count against the param count during invocation thus predictably leading to drops', function(){
-      // 7 is dropped, param count is 4 but 5 args given
-      r(pf(_,2,3,_,_,_)(_,4,5,6,7)(1))
-    });
-
     it('throws an error if more than one shoulder is used per partial');
-
     it('throws an error if used on variable-arguments function');
 
   });
