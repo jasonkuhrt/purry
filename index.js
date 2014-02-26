d1847c1cce62b5cf46e57b5d50a3a1120c9873cf
Jason Kuhrt <jasonkuhrt@me.com>
2014-02-25 23:29:39 -0500

    Use augment function to simply extend purry with intallation features

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 'use strict';
 var array_of = require('./lib/array-of');
 var stock_args = require('./lib/stock-args');
+var augment = require('./lib/augment');
 var syntax = require('./lib/syntax');
 var errors = require('./lib/errors');
 var Installer = require('./lib/installer');
 
 
 
+
 function purry(f){
   var psize = f.length;
   if (psize) {
-    return stock_args(f, array_of(psize, _), psize, 0, psize-1);
+    return stock_args(f, array_of(psize, syntax.hole.value), psize, 0, psize-1);
   } else {
      throw errors.no_vargs_support();
   }
@@ -24,17 +26,7 @@ purry.___ = syntax.hole.value;
 
 // Setup syntax installion commands.
 
-var installation = Installer.global(syntax.lookup);
-
-purry.installSyntax = installation.install;
-
-purry.noConflict = installation.uninstall;
-
-purry.install = function(mappings){
-  purry.installSyntax(mappings);
-  // TODO Expose purry on function prototype.
-  // Function.prototype.purry = purry;
-};
+augment(purry, Installer.global(syntax.lookup));
 
 // Install default global purry identifiers by default.
 // This assumes almost all purry users will want the default
@@ -42,6 +34,7 @@ purry.install = function(mappings){
 // it is, it follows that opt-out is better than opt-in.
 // purry.noConflict may always undo this.
 purry.install(syntax.lookup);
+
 
 
 
