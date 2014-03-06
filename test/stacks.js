31d6d15881cf26903868e460a9e01805aca6f682
Jason Kuhrt <jasonkuhrt@me.com>
2014-03-05 23:24:24 -0500

    Create purry_error that handles the business of creating stack-traces

--- test/stacks.js
+++ test/stacks.js
@@ -9,7 +9,15 @@ var padd = purry(add);
 var add1 = padd(1);
 
 // add1(2, ___, 'foo', ___);
-add1(___, module, 45, 45, 45);
+doit(add1)(___, module, 45, 45, 45);
+// add1(___, module, 45, 45, 45);
+
+function doit(f){
+  return function(){
+    f.apply(null, arguments);
+  };
+}
+
 // add1(___, module, ___, ___);
 // add1('1');
 
