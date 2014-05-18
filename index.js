1a8484c37cd50fb4b0e796856e21132424a5075d
Jason Kuhrt <jasonkuhrt@me.com>
2014-05-17 22:10:54 -0400

    Simplify installation mechanisms - closes #7

--- index.js
+++ index.js
@@ -1,45 +1,22 @@
 'use strict';
-var lu = require('./lib/utils/prelude'),
-    augment = lu.augment,
-    array_of = lu.array_of;
-var installer = require('./lib/utils/installer');
+
+var pe = require('./lib/utils/prelude'),
+    array_of = pe.array_of;
 var errors = require('./lib/errors');
+var algo_fixed = require('./lib/algo-fixed');
 var syntax = require('./lib/syntax');
-var stock_args = require('./lib/stock-args');
-
-
-
-function purry(f){
-  var psize = f.length;
-  if (psize) {
-    return stock_args(f, array_of(psize, syntax.tokens._), psize, 0, psize-1);
-  } else {
-     throw errors.no_vargs_support();
-  }
-}
-
-
-
-// Setup syntax global installion.
-
-var globalBootstrapper = installer.global(syntax.tokens);
-
-purry.install = function installPurry(mappings){
-  globalBootstrapper.install(mappings);
-  return purry;
-};
-
-purry.uninstall = function uninstallPurry(){
-  globalBootstrapper.uninstall();
-  return purry;
-};
-
-
-
-// Setup token access to support local installation.
-
-augment(syntax.tokens);
 
 
 
 module.exports = purry;
+
+function purry(f){
+  var psize = f.length;
+  if (psize) {
+    return algo_fixed(f, array_of(psize, syntax.hole.val), psize, 0, psize-1);
+  } else {
+    throw errors.no_vargs_support();
+  }
+}
+
+purry.install = require('./lib/install')(purry);
