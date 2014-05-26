f19cf2c970f2645553411597cd7ed0266ff8c2f7
Jason Kuhrt <jasonkuhrt@me.com>
2014-05-25 22:48:00 -0400

    Revise the purry_incomplete_holes error with purry_trailing_holes which is more complete; several ocd changes too

--- test-fuzzy/foo.js
+++ test-fuzzy/foo.js
@@ -10,8 +10,11 @@ var p = require('../install');
 var a = p(function(accumulator, index, collection){ return [accumulator, index, collection]; });
 
 // p(function(){ return arguments; });
-a(___, ___, ___);
-a(_);
-a(1, 2, 3, 4, _);
+// a(___, _, 5, _);
+a(5, _, _, ___);
+// a(_, ___, 5, _);
+// a(___, ___, ___);
+// a(_);
+// a(1, 2, 3, 4, _);
 // a(1,2,3,___)(_);
 // a(1,2,3,___)(_, ___);
